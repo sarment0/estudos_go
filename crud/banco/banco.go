11c2cca653076cc475c709f23dee80f53274eb77
Fabio Sarmento <fabio.nsarmento@gmail.com>
2023-04-14 11:35:48 -0300

    insert banco golang

---
+++ crud/banco/banco.go
@@ -0,0 +1,22 @@
+package banco
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Conectar() (*sql.DB, error) {
+	stringConexao := "root:12345@/devbook?charset=utf8&parseTime=True&loc=Local"
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		return nil, erro
+	}
+	fmt.Println("Sucesso ao conectar no banco!")
+	return db, nil
+}
